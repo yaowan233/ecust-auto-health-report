574eeb2f9f82a30f53c6f0a536fd19b2e0515bdd
lxl66566 <18259734087@163.com>
2022-10-03 09:46:22 +0800

    修复密码含有逗号无法打卡的bug

--- main.py
+++ main.py
@@ -39,7 +39,8 @@ def run(playwright: Playwright, stu_id, password) -> None:
 data = os.environ.get('ACCOUNT').strip().split(' ')  # 字符串预处理
 
 for i in range(0,len(data), 2):
-    account = data[i], password = data[i+1]
+    account = data[i]
+    password = data[i+1]
     try:
         with sync_playwright() as playwright:
             run(playwright, account.strip(), password.strip())
